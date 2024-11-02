1cd0db4d8beac1654d5453a0baadd8b74bb30c2c
Givi <Givi.givi.khaduri@gmail.com>
2024-11-02 12:11:18 +0400

    Initial commi32 saleforse

--- db.js
+++ db.js
@@ -1,8 +1,23 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+console.log('Строка подключения:', process.env.DATABASE_URL); // Выводим строку подключения
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
 
-module.exports = pool;
+// Тестирование подключения
+async function testConnection() {
+    try {
+        const client = await pool.connect();
+        console.log('Подключение к базе данных успешно');
+        client.release(); // Освобождаем клиента после теста
+    } catch (error) {
+        console.error('Ошибка подключения к базе данных:', error);
+    } finally {
+        await pool.end(); // Закрываем пул
+    }
+}
+
+testConnection();
